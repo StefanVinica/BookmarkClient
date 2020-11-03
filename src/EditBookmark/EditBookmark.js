5339241751ef6496660b1e9b09ee35f730646a50
StefanVinica <stefanikolovski996@gmail.com>
2020-11-03 00:31:18 -0800

    client commit

---
+++ src/EditBookmark/EditBookmark.js
@@ -0,0 +1,205 @@
+import React, { Component } from  'react';
+import PropTypes from 'prop-types';
+import BookmarksContext from '../BookmarksContext';
+import config from '../config'
+import './EditBookmark.css';
+
+const Required = () => (
+  <span className='EditBookmark__required'>*</span>
+)
+
+class EditBookmark extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.object,
+    }),
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,
+  };
+  
+  static contextType = BookmarksContext;
+
+  state = {
+    error: null,
+    id: '',
+    title: '',
+    url: '',
+    description: '',
+    rating: 1,
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const {bookmarkId} = this.props.match.params
+    const {id,title,url,description,rating} = this.state
+    const newBookmark = {id,title,url,description,rating}
+    fetch(`http://localhost:8080/api/bookmarks/${bookmarkId}`,{
+      method: 'PATCH',
+      body: JSON.stringify(newBookmark),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${config.API_KEY}`
+      }
+      })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(error => Promise.reject(error))
+      })
+      .then(() => {
+        this.resetFields(newBookmark)
+        this.context.editBookmark(newBookmark)
+        this.props.history.push('/')
+      })
+      .catch(error => {
+        console.error(error)
+        this.setState({ error })
+      })
+  }
+
+  resetFields = (newFields) => {
+    this.setState({
+      id: newFields.id || '',
+      title: newFields.title || '',
+      url: newFields.url || '',
+      description: newFields.description || '',
+      rating: newFields.rating || '',
+    })
+  }
+
+  componentDidMount(){
+    const {bookmarkId} = this.props.match.params
+    fetch(`http://localhost:8080/api/bookmarks/${bookmarkId}`,{
+    method: 'GET',
+    headers: {
+      'authorization': `Bearer ${config.API_KEY}`
+    }
+  })
+  .then(res => {
+    if (!res.ok) {
+      return res.json().then(error => Promise.reject(error))
+    }
+    return res.json()
+  })
+  .then(responseData => {
+    this.setState({
+      id: responseData.id,
+      title: responseData.title,
+      url: responseData.url,
+      description: responseData.description,
+      rating: responseData.rating,
+    })
+  })
+  .catch(error => {
+    console.error(error)
+    this.setState({ error })
+  })
+  }
+  handleChangeTitle = e => {
+    this.setState({ title: e.target.value })
+  };
+
+  handleChangeUrl = e => {
+    this.setState({ url: e.target.value })
+  };
+
+  handleChangeDescription = e => {
+    this.setState({ description: e.target.value })
+  };
+
+  handleChangeRating = e => {
+    this.setState({ rating: e.target.value })
+  };
+
+  handleClickCancel = () => {
+    this.props.history.push('/')
+  };
+
+  render() {
+    const { error } = this.state
+    return (
+      <section className='EditBookmark'>
+        <h2>Edit a bookmark</h2>
+        <form
+          className='EditBookmark__form'
+          onSubmit={this.handleSubmit}
+        >
+          <div className='EditBookmark__error' role='alert'>
+            {error && <p>{error.message}</p>}
+          </div>
+          <div>
+            <label htmlFor='title'>
+              Title
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              placeholder={`${this.state.title}`}
+              value={this.state.title}
+              onChange={this.handleChangeTitle}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='url'>
+              URL
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='url'
+              name='url'
+              id='url'
+              placeholder={`${this.state.url}`}
+              onChange={this.handleChangeUrl}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='description'>
+              Description
+            </label>
+            <textarea
+              name='description'
+              id='description'
+              value = {`${this.state.description}`}
+              onChange={this.handleChangeDescription}
+            />
+          </div>
+          <div>
+            <label htmlFor='rating'>
+              Rating
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='number'
+              name='rating'
+              id='rating'
+              min='1'
+              max='5'
+              value ={`${this.state.rating}`}
+              onChange={this.handleChangeRating}
+              required
+            />
+          </div>
+          <div className='EditBookmark__buttons'>
+            <button type='button' onClick={this.handleClickCancel}>
+              Cancel
+            </button>
+            {' '}
+            <button type='submit'>
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default EditBookmark;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  API_ENDPOINT: `http://localhost:8080/api/bookmarks`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
+
+console.log(process.env.REACT_APP_API_KEY)
